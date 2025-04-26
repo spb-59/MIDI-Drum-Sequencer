0538bd39eb3a4719fd14c242b92ec9b4edcad66a
spb-59 <shreeshailpbhatta@gmail.com>
2025-04-27 01:27:20 +0930

    Add USB MIDI support for knob control and update build flags

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,59 @@
 #include <Arduino.h>
+#include <USB_MIDI.h>
 
 
+int knobs_curr[3]={0,0,0};
+int knobs_prev[3]={0,0,0};
 
-
-  const int ledPin = 13;
-
-  // the setup() method runs once, when the sketch starts
   
   void setup() {
-    // initialize the digital pin as an output.
-    pinMode(ledPin, OUTPUT);
+    Serial.begin(57600);
+    analogReadAveraging(64);
   }
   
   // the loop() methor runs over and over again,
   // as long as the board has power
   
   void loop() {
-    digitalWrite(ledPin, HIGH);   // set the LED on
-    delay(1000);                  // wait for a second
-    digitalWrite(ledPin, LOW);    // set the LED off
-    delay(100);                  // wait for a second
+    knobs_curr[0]=analogRead(A14);
+    if (knobs_curr[0]!=knobs_prev[0]){
+      // Serial.print("Knob 1:");
+      // Serial.println(knobs_curr[0]);
+
+       usbMIDI.sendControlChange(1,knobs_curr[0]/8,1);
+       knobs_prev[0]=knobs_curr[0];
+
+    }
+    
+    delay(5);
+    knobs_curr[1]=analogRead(A0);
+    if (knobs_curr[1]!=knobs_prev[1]){
+      // Serial.print("Knob 2:");
+      // Serial.println(knobs_curr[1]);
+
+       usbMIDI.sendControlChange(2,knobs_curr[1]/8,1);
+       knobs_prev[1]=knobs_curr[1];
+
+
+    }
+    delay(5);
+
+    knobs_curr[2]=analogRead(A1);
+    if (knobs_curr[2]!=knobs_prev[2]){
+      Serial.print("Knob 3:");
+      Serial.println(knobs_curr[2]);
+
+       usbMIDI.sendControlChange(3,knobs_curr[2]/8,1);
+       knobs_prev[2]=knobs_curr[2];
+
+
+    }
+
+
+    delay(5);
+
+   
+
+
   }
   
