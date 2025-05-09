6aa9404ae76744d7cd61aacaad894ce3689deba3
spb-59 <shreeshailpbhatta@gmail.com>
2025-05-09 15:55:53 +0930

    Refactor MIDI handling and add scales functionality for improved note processing

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,39 @@
 #include <Arduino.h>
 #include <USB_MIDI.h>
+#include <scales.h>
 
 
-int knobs_curr[3]={0,0,0};
-int knobs_prev[3]={0,0,0};
-int notes[] = { 63,60,61, 67,69,67,61,60,63};
-int curr_note=-1;
-bool sig = false;
-bool onOFF = false;
+int knobs_curr[6]={0,0,0,0,0,0};
+int knobs_prev[6]={0,0,0,0,0,0};
+int knobs[5]={A0,A1,A2,A3,A4};
 int velocity = 127;
-int channel = 1;
-bool sustain = false;
-int interval=5;
-bool start=false;
-bool playing = false;
-unsigned long lastClock = 0;
+int delay_time=10;
+int offset=0;
 
 
 
-
-void arp() {
-  curr_note = curr_note<8? curr_note+1:0;     
-  int velocity = random(80, 92);        
-
-  int interval = (6000/87)*1;              
-
-  usbMIDI.sendNoteOn(notes[curr_note], velocity, channel);
-  delay(interval*0.8 );                
-  usbMIDI.sendNoteOff(notes[curr_note], 0, channel);
-  delay(interval*0.2);                
+void readKnob(int knobIndex, int analogPin) {
+  knobs_curr[knobIndex] = analogRead(analogPin);
+  if (knobs_curr[knobIndex] != knobs_prev[knobIndex]) {
+    Serial.print("Knob ");
+    Serial.print(knobIndex + 1);
+    Serial.print(": ");
+    Serial.println(knobs_curr[knobIndex]);
+    knobs_prev[knobIndex] = knobs_curr[knobIndex];
+  }
+  delay(1);
+}
+void sendMidi(int i) {
+  int data = constrain(map(knobs_curr[i], 0, 1023, 60, 72), 60, 72);
+  data = scales.minor(data)+offset;
+  usbMIDI.sendNoteOn(data, velocity, 1);
+  delay(delay_time);
+  usbMIDI.sendNoteOff(data, velocity, 1);
 }
 
-
-  void setup() {
+void setup() {
     Serial.begin(57600);
     analogReadAveraging(64);
-    pinMode(33, INPUT_PULLUP);
-    pinMode(34, INPUT_PULLUP);
-    pinMode(35, INPUT_PULLUP);
   }
   
   // the loop() methor runs over and over again,
@@ -45,88 +41,17 @@ void arp() {
   
   void loop() {
 
+    for (int i=0;i<4;i++){
+      readKnob(i,knobs[i]);
+      readKnob(4,4);
+      readKnob(5,5);
 
-    knobs_curr[0]=analogRead(A0);
-    if (knobs_curr[0]!=knobs_prev[0]){
-      // Serial.print("Knob 1:");
-      // Serial.println(knobs_curr[0]);
+      velocity=random(50,120);
+      delay_time=constrain(map(knobs_curr[4],0,1023,120,200),80,300);
+      offset=scales.minor(constrain(map(knobs_curr[5],0,1023,-12,12),-12,12));
 
-       usbMIDI.sendControlChange(1,knobs_curr[0]/8,channel);
-       knobs_prev[0]=knobs_curr[0];
-
-    }
-    
-    delay(5);
-    knobs_curr[1]=analogRead(A2);
-    if (knobs_curr[1]!=knobs_prev[1]){
-      // Serial.print("Knob 2:");
-      // Serial.println(knobs_curr[1]);
-
-       usbMIDI.sendControlChange(2,knobs_curr[1]/8,channel);
-       knobs_prev[1]=knobs_curr[1];
-    }
-    delay(5);
-
-    int data = digitalRead(33);
-
-    if (onOFF) {
-      arp();
-      digitalWrite(13, 1);
-    } else {
-      digitalWrite(13, 0);
-    }
-  
-    if (data == LOW && !sig) {
-      // Serial.print("Button Pressed");
-      sig = true;
-      onOFF = !onOFF;
-      delay(2);
-    }
-  
-    if (data == HIGH && sig) {
-      sig = false;
-      delay(2);
+      sendMidi(i);
     }
 
-
-    //sustain pedal on/off
-    int sustainData = digitalRead(34);
-    if (sustainData == LOW && !sustain) {
-      usbMIDI.sendControlChange(64,100,channel);
-      sustain = true;
-      delay(2);
-    }
-    
-    if (sustainData == HIGH && sustain) {
-    
-      usbMIDI.sendControlChange(64,0,channel);
-      sustain = false;
-      delay(2);
-    }
-
-    int startData = digitalRead(35);
-    if (startData == LOW && !start) {
-
-        Serial.println("Button Pressed: START");
-        usbMIDI.sendRealTime(usbMIDI.Start); // start from beginning
-        start = true;
-
-
-      delay(2); 
-    }
-    if (startData == HIGH && start) {
-      start = false;
-
-      delay(2);
-    }
-  
-    if (start) {
-      unsigned long now = millis();
-      if (now - lastClock >= 14.45) { 
-        usbMIDI.sendRealTime(usbMIDI.Clock);
-        lastClock = now;
-      }
-    }
     usbMIDI.read();
   }
-  
