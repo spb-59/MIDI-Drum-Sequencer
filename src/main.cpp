6fd729e9e42bc8e2120a1bdb225a880cee190059
spb-59 <shreeshailpbhatta@gmail.com>
2025-05-13 00:35:51 +0930

    ws8

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,21 @@
 #include <USB_MIDI.h>
 #include <scales.h>
 
-
 int knobs_curr[6]={0,0,0,0,0,0};
 int knobs_prev[6]={0,0,0,0,0,0};
-int knobs[5]={A0,A1,A2,A3,A4};
+int knobs[6]={A0,A1,A2,A3,A4,A5};
 int velocity = 127;
 int delay_time=10;
-int offset=0;
+int current_pitch_bend = 0;
+int prev_pitch_bend = 0;
 
 
+unsigned long previousTime = 0;
+unsigned long previousLEDTime = 0;
+unsigned long ledInterval = 200; 
+int currentKnob = 0;
+bool isNoteOn = false;
+int currentNote = 0;
 
 void readKnob(int knobIndex, int analogPin) {
   knobs_curr[knobIndex] = analogRead(analogPin);
@@ -21,37 +27,66 @@ void readKnob(int knobIndex, int analogPin) {
     Serial.println(knobs_curr[knobIndex]);
     knobs_prev[knobIndex] = knobs_curr[knobIndex];
   }
-  delay(1);
-}
-void sendMidi(int i) {
-  int data = constrain(map(knobs_curr[i], 0, 1023, 60, 72), 60, 72);
-  data = scales.minor(data)+offset;
-  usbMIDI.sendNoteOn(data, velocity, 1);
-  delay(delay_time);
-  usbMIDI.sendNoteOff(data, velocity, 1);
 }
 
 void setup() {
     Serial.begin(57600);
     analogReadAveraging(64);
+    pinMode(13, OUTPUT);
+    
+}
+  
+void loop() {
+  unsigned long currentTime = millis();
+  
+
+  for (int i = 0; i < 6; i++) {
+    readKnob(i, knobs[i]);
   }
   
-  // the loop() methor runs over and over again,
-  // as long as the board has power
+  velocity = random(50, 120);
+  delay_time = constrain(map(knobs_curr[4], 0, 1023, 50, 300), 50, 300);
   
-  void loop() {
+ 
+  int pitch_bend = map(knobs_curr[5], 0, 1023, -8192, 8191);
+  
 
-    for (int i=0;i<4;i++){
-      readKnob(i,knobs[i]);
-      readKnob(4,4);
-      readKnob(5,5);
+  //to avoid noise in pitch bend
+  if (abs(pitch_bend) < 500) {
+    current_pitch_bend = 0; 
+  } else {
+    current_pitch_bend = pitch_bend;
+  }
+  
+  
+  if (abs(current_pitch_bend - prev_pitch_bend) > 200) {
+    usbMIDI.sendPitchBend(current_pitch_bend, 1);
+    prev_pitch_bend = current_pitch_bend;
+  }
+  
 
-      velocity=random(50,120);
-      delay_time=constrain(map(knobs_curr[4],0,1023,120,200),80,300);
-      offset=scales.minor(constrain(map(knobs_curr[5],0,1023,-12,12),-12,12));
-
-      sendMidi(i);
+  if (currentTime - previousTime >= delay_time) {
+    previousTime = currentTime;
+    
+    if (isNoteOn) {
+      // ff note is on, turn it off and change to next knob
+      usbMIDI.sendNoteOff(currentNote, velocity, 1);
+      isNoteOn = false;
+      
+      currentKnob = (currentKnob + 1) % 4;
+    } else {
+  
+      currentNote = constrain(map(knobs_curr[currentKnob], 0, 1023, 60, 72), 60, 72);
+      currentNote = scales.minor(currentNote);
+      usbMIDI.sendNoteOn(currentNote, velocity, 1);
+      isNoteOn = true;
     }
-
-    usbMIDI.read();
   }
+
+  // led hould be independent of midi
+  if (currentTime - previousLEDTime >= ledInterval) {
+    previousLEDTime = currentTime;
+    digitalWriteFast(13, !digitalReadFast(13)); 
+  }
+  
+}
